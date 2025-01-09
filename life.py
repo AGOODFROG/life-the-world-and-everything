0d670227cb7cb695758bf7153a37ef9e404ba000
AGOODFROG <cadenjquirion@gmail.com>
2025-01-08 20:18:37 -0500

    added death logic

--- life.py
+++ life.py
@@ -1,26 +1,14 @@
 class life:
     def __init__(self, cols, rows):
-        #self.next_grid = [] # set to an empty list to avoid wirrd stuff happening
         self.grid =  [[0 for i in range(cols)] for j in range(rows)]
         self.livingCellPoscitons = []# living cel
+        #TODO fix dubliation in deadNeighorCells
         self.deadNeighborCells = [] # if a dead cell has three neighbors it is born, no need for dublicates just slows things down 
-        
-        '''
-        for i in range(length):
-            self.grid.append([])
-            for j in range(width):
-                self.grid[-1].append(0)
-        '''
-      
-        
-        
-            
-       
+
     def __str__(self):
         #TODO fix bug that prints as an array 
         return self.formatGrid(self.grid)
-
-        
+    # reduant?    
     def formatGrid(self, grid):
          out = ""
          for i in range(len(grid)):
@@ -40,7 +28,7 @@ class life:
     """
     def addCell(self, row, column):
         self.grid[row][column] = 1
-        self.livingCellPoscitons.append([row, column]) # appends to a list witch is a value in a dict
+        self.livingCellPoscitons.append([row, column]) 
     
     # make method that adds a list of cells
     def addCellList(self, array):
@@ -63,11 +51,11 @@ class life:
         collnmum = postion[1]
         out = 0
         #print(collnmum)
-        cell_postions =[[row+1, collnmum+1],[row+1, collnmum],[row-1, collnmum+1],
-                        [row, collnmum+1],                      [row, collnmum-1],
-                        [row+1, collnmum-1],[row-1, collnmum],[row-1, collnmum-1],]
+        cell_postions =[(row+1, collnmum+1),(row+1, collnmum),(row-1, collnmum+1),
+                        (row, collnmum+1),                      (row, collnmum-1),
+                        (row+1, collnmum-1),(row-1, collnmum),(row-1, collnmum-1),]
         for cell in cell_postions:
-            #TODO implent add dead neibor
+            #TODO FIX DUPLATION IN ADDDEADNEIBOR
             row = cell[0]
             col = cell[1]
             if self.grid[row][col] == 1:
@@ -77,29 +65,24 @@ class life:
         return out
 
     # returns cells to kill
+    #DOSE NOT KILL CELLS
     def runDeathLogic(self):
-       # get list of living cell to kill
-       #print(self.livingCellPoscitons)
         cell_to_kill = []
-       # go over each cell that may or may not be killed
         for cell in self.livingCellPoscitons:
-            #print(cell, self.getNabors(cell))
-            #checks if should live
+            #checks if should die
             if self.getNabors(cell) != 2 and self.getNabors(cell) != 3:
                 #store cell so it can be  killed by iterate life
-               
                 cell_to_kill.append(cell)
-                
-            
-        print("next cell gen:",cell_to_kill)
+
         return cell_to_kill
+    
+    def birthLogic(self):
+        # keep log of cells to be born
+        cells_to_birth = []
+        # iterate of possible cells to be born
 
+        #return cells to be born
 
-       # if cell should live copy to new grid of all dead cells
-
-
-
-       # if not do nothing
 
 
     
@@ -109,8 +92,8 @@ myLife = life(10,10)
 
 #myLife.addCell(1,1)
 #myLife.addCell(2,2)
-#myLife.addCell(2,3)
-#myLife.addCell(3,2)
+myLife.addCell(2,3)
+myLife.addCell(3,2)
 myLife.addCell(3,3)
 
 
